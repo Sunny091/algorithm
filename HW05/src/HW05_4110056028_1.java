18551c5cf20ee7b56a6e2b9c5d4936d85fa32054
Sunny019 <s890919@gmail.com>
2023-03-30 23:24:16 +0800

    second version

--- HW05/src/HW05_4110056028_1.java
+++ HW05/src/HW05_4110056028_1.java
@@ -2,9 +2,13 @@ public class HW05_4110056028_1 extends littlebit {
     public static void main(String[] args) throws Exception {
         int[] A = { 0, 1, 3, 3, 5, 2 };
         int[] B = { 2, 5, 1, 6, 4, 8 };
+        int[] C = { 1, 0, 2 };
+        int[] D = { 1, 2, 2 };
         HW05_4110056028_1 test = new HW05_4110056028_1();
         System.out.println(test.l_bit(A));
         System.out.println(test.l_bit(B));
+        System.out.println(test.l_bit(C));
+        System.out.println(test.l_bit(D));
     }
 
     @Override
@@ -23,7 +27,7 @@ public class HW05_4110056028_1 extends littlebit {
         }
         for (int i = A.length - 2; i > -1; i--) {
             if (A[i] > A[i + 1]) {
-                if (A[i] < A[i - 1]) {
+                if (reward[i] <= reward[i + 1]) {
                     reward[i] = reward[i + 1] + 1;
                 }
             } else if (A[i] == A[i + 1]) {
@@ -31,7 +35,7 @@ public class HW05_4110056028_1 extends littlebit {
             }
         }
         // for (int i : reward) {
-        //     System.out.print(i + " ");
+        // System.out.print(i + " ");
         // }
         for (int i : reward) {
             result += i;
