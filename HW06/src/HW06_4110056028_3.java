d8be21896e9b4983c49d3ec4ebde6e93b08a9ce1
Sunny Shen <s890919@gmail.com>
2023-04-07 17:30:13 +0800

    version 3-2

--- HW06/src/HW06_4110056028_3.java
+++ HW06/src/HW06_4110056028_3.java
@@ -17,10 +17,37 @@ public class HW06_4110056028_3 extends MedianOfArrays {
                 temp[i] = arrays[i][(arrays[i].length - 1) / 2];
             }
         }
+        quickSort(temp, 0, temp.length - 1);
         if (temp.length % 2 == 0) {
             return (temp[temp.length / 2 - 1] + temp[temp.length / 2]) / 2;
         } else {
             return temp[(temp.length - 1) / 2];
         }
+    }
+
+    private void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+        return i + 1;
     }
 }
