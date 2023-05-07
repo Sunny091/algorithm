48aa4e2d1ab82ad49006f933e5fedb0ff35de126
Sunny019 <s890919@gmail.com>
2023-05-07 20:32:25 +0800

    version 2 modify version by changing direction order

---
+++ HW08/src/HW08_4110056028_2.java
@@ -0,0 +1,39 @@
+public class HW08_4110056028_2 extends MaxLandArea {
+
+    private int[] directionX = { 1, 0, -1, 0 };
+    private int[] directionY = { 0, -1, 0, 1 };
+
+    private int answer = 0;
+
+    @Override
+    public int maxLandArea(int[][] array) {
+        int[][] record = new int[array.length][array[0].length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == 1) {
+                    if (record[i][j] == 0) {
+                        run(array, record, i, j, 1);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    private void run(int[][] array, int[][] record, int nowX, int nowY, int count) {
+        if (count > answer) {
+            answer = count;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (nowX + directionX[i] > -1 && nowX + directionX[i] < array[0].length && nowY + directionY[i] > -1
+                    && nowY + directionY[i] < array.length) {
+                if (array[nowX + directionX[i]][nowY + directionY[i]] == 1) {
+                    if (record[nowX + directionX[i]][nowY + directionY[i]] == 0) {
+                        record[nowX + directionX[i]][nowY + directionY[i]] = 1;
+                        run(array, record, nowX + directionX[i], nowY + directionY[i], ++count);
+                    }
+                }
+            }
+        }
+    }
+}
--- HW08/src/test.java
+++ HW08/src/test.java
@@ -5,7 +5,7 @@ public class test {
 
     @Test
     void MaxLandArea() {
-        HW08_4110056028_1 test = new HW08_4110056028_1();
+        HW08_4110056028_2 test = new HW08_4110056028_2();
         int[][] star1 = { { 0, 1, 1 },
                 { 1, 0, 1 },
                 { 0, 1, 1 } };
