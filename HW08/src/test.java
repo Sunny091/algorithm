d07141cbcb21efae06ffbe195c02585b9d2e06cb
Sunny019 <s890919@gmail.com>
2023-05-07 20:27:22 +0800

    version 1

---
+++ HW08/src/test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class test {
+
+    @Test
+    void MaxLandArea() {
+        HW08_4110056028_1 test = new HW08_4110056028_1();
+        int[][] star1 = { { 0, 1, 1 },
+                { 1, 0, 1 },
+                { 0, 1, 1 } };
+        int actual1 = test.maxLandArea(star1);
+        Assertions.assertEquals(5, actual1);
+    }
+}
