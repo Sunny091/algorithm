99744f569ae497c73736a088de0757c11010c757
Sunny Shen <s890919@gmail.com>
2023-05-09 17:52:31 +0800

    version 2 record before running

--- HW08/src/HW08_4110056028_2.java
+++ HW08/src/HW08_4110056028_2.java
@@ -8,29 +8,46 @@ public class HW08_4110056028_2 extends MaxLandArea {
     @Override
     public int maxLandArea(int[][] array) {
         int[][] record = new int[array.length][array[0].length];
+        for (int i = 0; i < record.length; i++) {
+            for (int j = 0; j < record[0].length; j++) {
+                record[i][j] = 0;
+            }
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (array[i][j] == 1) {
                     if (record[i][j] == 0) {
+                        record[i][j] = 1;
                         run(array, record, i, j, 1);
                     }
                 }
             }
         }
+        for (int i = 0; i < record.length; i++) {
+            for (int j = 0; j < record[0].length; j++) {
+                System.out.print(record[i][j] + " ");
+            }
+            System.out.println("");
+        }
         return answer;
     }
 
     private void run(int[][] array, int[][] record, int nowX, int nowY, int count) {
-        if (count > answer) {
-            answer = count;
-        }
+        // if (count > answer) {
+        // answer = count;
+        // }
         for (int i = 0; i < 4; i++) {
-            if (nowX + directionX[i] > -1 && nowX + directionX[i] < array[0].length && nowY + directionY[i] > -1
-                    && nowY + directionY[i] < array.length) {
-                if (array[nowX + directionX[i]][nowY + directionY[i]] == 1) {
-                    if (record[nowX + directionX[i]][nowY + directionY[i]] == 0) {
-                        record[nowX + directionX[i]][nowY + directionY[i]] = 1;
-                        run(array, record, nowX + directionX[i], nowY + directionY[i], ++count);
+            int now_x = nowX + directionX[i];
+            int now_y = nowY + directionY[i];
+            if (now_x > -1 && now_x < array.length && now_y > -1 && now_y < array[0].length) {
+                if (record[now_x][now_y] == 0) {
+                    if (array[now_x][now_y] == 1) {
+                        record[now_x][now_y] = 1;
+                        count++;
+                        if (count > answer) {
+                            answer = count;
+                        }
+                        run(array, record, now_x, now_y, count);
                     }
                 }
             }
--- HW08/src/test.java
+++ HW08/src/test.java
@@ -6,10 +6,20 @@ public class test {
     @Test
     void MaxLandArea() {
         HW08_4110056028_2 test = new HW08_4110056028_2();
-        int[][] star1 = { { 0, 1, 1 },
+        int[][] start1 = { { 0, 1, 1 },
                 { 1, 0, 1 },
                 { 0, 1, 1 } };
-        int actual1 = test.maxLandArea(star1);
+        int[][] start2 = { { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } };
+        int actual1 = test.maxLandArea(start1);
+        int actual2 = test.maxLandArea(start2);
         Assertions.assertEquals(5, actual1);
+        Assertions.assertEquals(6, actual2);
     }
 }
