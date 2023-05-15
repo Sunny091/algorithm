6d41558f829d82f7e17027eb7762c057ac042e1a
Sunny019 <s890919@gmail.com>
2023-05-15 10:49:48 +0800

    version 1

--- HW09/src/HW09_4110056028_1.java
+++ HW09/src/HW09_4110056028_1.java
@@ -6,6 +6,14 @@ public class HW09_4110056028_1 extends LSD {
 
     @Override
     public int Distance(int[][] array) {
-        
+        int limit = 1000000;
+        ArrayList<Integer>[] arrayLists = new ArrayList<Integer>[limit];
+        for (ArrayList<Integer> graph : arrayLists) {
+            graph = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < array.length; i++) {
+            arrayLists[array[i][0]].add(array[i][1]);
+            arrayLists[array[i][1]].add(array[i][0]);
+        }
     }
 }
