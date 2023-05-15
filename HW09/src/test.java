6cc3d2b3245fe611918781f81b44efdb110605f2
Sunny Shen <s890919@gmail.com>
2023-05-15 18:07:44 +0800

    version 2 by andy

--- HW09/src/test.java
+++ HW09/src/test.java
@@ -5,7 +5,7 @@ public class test {
 
     @Test
     void LSD() {
-        HW09_4110056028_1 test = new HW09_4110056028_1();
+        HW09_4110056028_2 test = new HW09_4110056028_2();
         int[][] start1 = { { 0, 1 }, { 0, 2 }, { 0, 4 }, { 1, 3 }, { 1, 4 }, { 2, 5 }, { 6, 7 } };
         int actual1 = test.Distance(start1);
         Assertions.assertEquals(4, actual1);
