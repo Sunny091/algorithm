7da81661cd75936d922b1815e208dfa8cfff55be
Sunny019 <s890919@gmail.com>
2023-05-18 00:01:55 +0800

    version 1 creat graph structure

--- HW09/src/HW09_4110056028_1.java
+++ HW09/src/HW09_4110056028_1.java
@@ -6,15 +6,50 @@ public class HW09_4110056028_1 extends LSD {
 
     @Override
     public int Distance(int[][] array) {
-        int limit = 1000000;
-        ArrayList<Integer>[] arrayLists = new ArrayList<Integer>[limit];
-        for (ArrayList<Integer> graph : arrayLists) {
-            graph = new ArrayList<Integer>();
-        }
+        ArrayList<node> graph = new ArrayList<node>();
+        int[] get = new int[1000000];
         for (int i = 0; i < array.length; i++) {
-            arrayLists[array[i][0]].add(array[i][1]);
-            arrayLists[array[i][1]].add(array[i][0]);
+            if (get[array[i][0]] == 1) {
+                for (node node : graph) {
+                    if (node.key == array[i][0]) {
+                        node.adj.add(array[i][1]);
+                    }
+                }
+            } else {
+                node e1 = new node(array[i][0]);
+                graph.add(e1);
+            }
+            if (get[array[i][1]] == 1) {
+                for (node node : graph) {
+                    if (node.key == array[i][1]) {
+                        node.adj.add(array[i][0]);
+                    }
+                }
+            } else {
+                node e2 = new node(array[i][1]);
+                graph.add(e2);
+            }
         }
         
+    }
+
+    class node {
+        private ArrayList<Integer> adj;
+        private int key;
+        private boolean hasVisited;
+
+        public node(int key) {
+            this.key = key;
+            this.adj = new ArrayList<>();
+            this.hasVisited = false;
+        }
+
+        public void add(int value) {
+            this.adj.add(value);
+        }
+
+        public void hasVisited() {
+            this.hasVisited = true;
+        }
     }
 }
