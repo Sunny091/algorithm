113e18f3ba9633bf91bcaf559a348a888d61390e
Sunny019 <s890919@gmail.com>
2023-06-02 09:58:19 +0800

    HW 10

--- HW10/src/HW10_4110056028_1.java
+++ HW10/src/HW10_4110056028_1.java
@@ -1,17 +1,20 @@
 public class HW10_4110056028_1 extends Buy_Phone {
 
-    private int[] yValue;
-
-    public HW10_4110056028_1() {
-        this.yValue = new int[1000000];
-    }
-
     @Override
-    public int[][] bestPhone(int[][] inputArr) {
-        for (int i = 0; i < inputArr.length; i++) {
-            if (inputArr[i][1] > this.yValue[inputArr[i][0]]) {
-                this.yValue[inputArr[i][0]] = inputArr[i][1];
-            }
-        }
-    }
+    public int[][] bestPhone(final int[][] A){
+		final int LEN=A.length, END=LEN-1;
+		int i, j, len=LEN;
+		for(i=0; i<END; ++i){
+			for(j=END; j>i; --j){
+				if(A[j]==null) continue;
+				else if(A[i]==null) { len--; break; }
+				else if(A[j][0]>=A[i][0]&&A[j][1]>=A[i][1])
+				{ A[i]=null; len--; break;}
+			}
+		}
+		final int[][] ANS=new int[len][];
+		for(i=j=0; i < LEN; ++i)
+			if(A[i]!=null){ ANS[j++]=A[i]; }
+		return ANS;
+	}
 }
